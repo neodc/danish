9f9ebd2b64350ab1024f7a1b59c1bf74d8feba3e
Loup <Loup@192.168.1.7>
2014-09-24 17:50:42 +0200

    Tab.

--- src/danish/business/Danish.java
+++ src/danish/business/Danish.java
@@ -3,8 +3,7 @@ package danish.business;
 public class Danish{
 
 	public static void main( String[] args ){
-		          System.out.println("Yolo");
-
+		System.out.println("Yolo");
 		// TODO code application logic here
 		System.out.print("hi");
 	}
