8839febc3d5f9c59802be297ade4e631f59ac5dd
neodc <noe@lal.be>
2014-11-20 21:30:50 +0100

    FIX Bug if the name is too long. #5

--- src/danish/view/NewGame.java
+++ src/danish/view/NewGame.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.BorderFactory;
@@ -126,6 +128,14 @@ public class NewGame extends JDialog {
 				sendInfo = false;
 			}
 
+		});
+		
+		
+		this.nameField.addKeyListener( new KeyAdapter() {
+			@Override
+			public void keyReleased( KeyEvent e ){
+				nameField.setText( nameField.getText().substring(0, 30) );
+			}
 		});
 	}
 
